6fdc82a8d63b5e52b46f007cf72db3174f387ca6
ishika <ishikarana1201@gmail.com>
2021-12-17 15:00:51 +0530

    second commit

--- index.php
+++ index.php
@@ -40,11 +40,29 @@
       <?php
 
       include "connection.php";
-     
-    $query = "SELECT *FROM students";
-    $sql = mysqli_query($con,$query);
+      $start = 0;
+      $per_page = 3;
+      $current_page = 1;
+      if(isset($_GET['start'])){
+        $start=$_GET['start'];
+        if($start<=0){
+          $start=0;
+      $current_page = 1;
 
+        }else{
+      $current_page = $start;
+        $start--;
+        $start = $start*$per_page;
+      }
+    }
+      $record = mysqli_num_rows(mysqli_query($con,"SELECT *FROM students"));
+      $pagi = ceil($record/$per_page);
+    
+    $query = "SELECT *FROM students limit $start,$per_page";
+    $sql = mysqli_query($con,$query);
+    if(mysqli_num_rows($sql)>0){
     while($row = mysqli_fetch_array($sql)){
+      
         ?>
      <tr>
       <td><?php echo $row['id']?></td>
@@ -61,11 +79,39 @@
           <a href="delete.php?id=<?php echo $row['id']?>" class="btn btn-danger"><i class="fa fa-trash"></i></a>
       </div></td>
       </tr>
-   <?php } ?>
-
+   <?php } }else{?>
+No Records
+<?php } ?>
   </tbody>
 </table>
-        
+<nav aria-label="...">
+  <ul class="pagination justify-content-center">
+    <?php
+    if($current_page> 1)
+    { ?>
+    <li class="page-item">
+      <a class="page-link" href="?start=<?php echo ($current_page-1) ?>" tabindex="-1" aria-disabled="true">Previous</a>
+    </li>
+    <?php } ?>
+
+    <?php 
+    
+    for($i=1;$i<=$pagi;$i++){
+      $class='';
+      if($current_page==$i){
+        $class='active';
+      } ?>
+    <li class="page-item <?php echo $class?>"><a class="page-link" href="?start=<?php echo $i ?>"><?php echo $i?></a></li>
+      <?php } ?>
+      <?php
+    if($i>$current_page)
+    { ?>
+    <li class="page-item">
+      <a class="page-link" href="?start=<?php echo ($current_page+1) ?>">Next</a>
+    </li>
+    <?php } ?>
+  </ul>
+</nav>  
     </div>
 <script src="js/bootstrap.js"></script>
   
